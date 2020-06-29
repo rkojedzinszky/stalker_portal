5481ff77df169209e6d519599098e2fe0eaa084b
Richard Kojedzinszky <richard.kojedzinszky@euronetrt.hu>
2020-06-29 14:47:29 +0200

    db: add pkey to changelog

--- deploy/src/init_db.sql
+++ deploy/src/init_db.sql
@@ -2,10 +2,12 @@
 USE `@DB_NAME@`;
 
 CREATE TABLE IF NOT EXISTS `changelog` (
+  `id` int NOT NULL auto_increment,
   `change_number` BIGINT NOT NULL,
   `delta_set` VARCHAR(10) NOT NULL,
   `start_dt` TIMESTAMP NOT NULL,
   `complete_dt` TIMESTAMP NULL,
   `applied_by` VARCHAR(100) NOT NULL,
-  `description` VARCHAR(500) NOT NULL
+  `description` VARCHAR(500) NOT NULL,
+  PRIMARY KEY (`id`)
 );
